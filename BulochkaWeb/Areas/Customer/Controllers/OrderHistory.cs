b9a0291fbd55e51b57171d7a8cd8365e0b981cea
Lev Logvinov <llogvinov356@gmail.com>
2023-03-26 23:10:23 +0300

    Added OrderHistory

---
+++ BulochkaWeb/Areas/Customer/Controllers/OrderHistory.cs
@@ -0,0 +1,29 @@
+﻿using Bulochka.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulochkaWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderHistory : Controller
+    {
+        private readonly IUnitOfWork _unitofwork;
+
+        public OrderHistory(IUnitOfWork unitOfWork)
+        {
+            _unitofwork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var ordersList = _unitofwork.OrderHeader.GetAll();
+
+            return View(ordersList);
+        }
+
+    }
+}
