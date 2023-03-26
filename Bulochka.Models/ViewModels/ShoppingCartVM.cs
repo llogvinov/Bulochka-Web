1e7ba0f552c324b4bccd81c6344acad3371943d3
Lev Logvinov <llogvinov356@gmail.com>
2023-03-26 22:27:58 +0300

    Added ability to place order to take in cafe

--- Bulochka.Models/ViewModels/ShoppingCartVM.cs
+++ Bulochka.Models/ViewModels/ShoppingCartVM.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +12,7 @@ namespace Bulochka.Models.ViewModels
     {
         public IEnumerable<ShoppingCart> ListCart { get; set; }
         public OrderHeader OrderHeader { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CompanyBranchList { get; set; }
     }
 }
--- BulochkaWeb/Areas/Customer/Controllers/CartController.cs
+++ BulochkaWeb/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using Bulochka.Models.ViewModels;
 using Bulochka.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Security.Claims;
 
 namespace BulochkaWeb.Areas.Customer.Controllers
@@ -80,6 +81,84 @@ namespace BulochkaWeb.Areas.Customer.Controllers
             ShoppingCartVM.OrderHeader.StreetAddress = ShoppingCartVM.OrderHeader.ApplicationUser.StreetAddress;
             ShoppingCartVM.OrderHeader.City = ShoppingCartVM.OrderHeader.ApplicationUser.City;
             ShoppingCartVM.OrderHeader.PostalCode = ShoppingCartVM.OrderHeader.ApplicationUser.PostalCode;
+            ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
+            ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+            ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+            ShoppingCartVM.OrderHeader.ApplicationUserId = claim.Value;
+
+            foreach (var cart in ShoppingCartVM.ListCart)
+            {
+                ShoppingCartVM.OrderHeader.OrderTotal += (cart.Product.Price * cart.Count);
+            }
+
+            _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+            _unitOfWork.Save();
+
+            foreach (var cart in ShoppingCartVM.ListCart)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    ProductId = cart.ProductId,
+                    OrderId = ShoppingCartVM.OrderHeader.Id,
+                    Price = cart.Product.Price,
+                    Count = cart.Count,
+                };
+
+                _unitOfWork.OrderDetail.Add(orderDetail);
+                _unitOfWork.Save();
+            }
+
+            _unitOfWork.ShoppingCart.RemoveRange(ShoppingCartVM.ListCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult SummaryInCafe()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+                    includeProperties: "Product"),
+                OrderHeader = new(),
+                CompanyBranchList = _unitOfWork.CompanyBranch.GetAll().Select(
+                    b => new SelectListItem
+                    {
+                        Text = b.City + ", " + b.StreetAddress,
+                        Value = b.Id.ToString()
+                    })
+            };
+            ShoppingCartVM.OrderHeader.ApplicationUser =
+                _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value);
+
+            foreach (var cart in ShoppingCartVM.ListCart)
+            {
+                ShoppingCartVM.OrderHeader.OrderTotal += (cart.Product.Price * cart.Count);
+            }
+
+            return View(ShoppingCartVM);
+        }
+
+        [HttpPost]
+        [ActionName("SummaryInCafe")]
+        [ValidateAntiForgeryToken]
+        public IActionResult SummaryPOSTInCafe(ShoppingCartVM ShoppingCartVM)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVM.ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+                includeProperties: "Product");
+            
+            var companyBranch = _unitOfWork.CompanyBranch.GetFirstOrDefault(
+                b => b.Id == ShoppingCartVM.OrderHeader.PickUpPlaceId);
+            ShoppingCartVM.OrderHeader.City = companyBranch.City;
+            ShoppingCartVM.OrderHeader.StreetAddress = companyBranch.StreetAddress;
+            ShoppingCartVM.OrderHeader.PostalCode = companyBranch.PostalCode;
+           
             ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
             ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
             ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
